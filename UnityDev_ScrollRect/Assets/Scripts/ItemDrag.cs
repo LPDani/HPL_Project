58c1a90564f7abce9c5f538496b75f8a6a7f1607
LPDani <v.dan19921119@gmail.com>
2015-10-13 21:16:21 +0200

    kepek behuzasa, diak megkezdese

---
+++ UnityDev_ScrollRect/Assets/Scripts/ItemDrag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ItemDrag : MonoBehaviour//, IPointerClickHandler
+{
+   /* public void OnPointerClick(PointerEventData aEventData)
+    {
+        if (aEventData.button == PointerEventData.InputButton.Left)
+        Debug.Log(aEventData.selectedObject.name);
+        Debug.Log("sajt");
+    }
+    */
+	// Use this for initialization
+    float distance;
+	void Start () {
+        distance = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+    void OnMouseDrag() {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(mousePos);
+        newPos.z = distance;
+        transform.position = newPos;
+    }
+}
